dbc91403b585455472c0b3ba4e149dc2695d7d97
SarayShlomi <saraysara1996@gmail.com>
2022-11-17 14:44:57 +0200

    start controller and guest

---
+++ src/main/java/chatApp/Entities/Guest.java
@@ -0,0 +1,32 @@
+package chatApp.Entities;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "guest")
+public class Guest {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private int id;
+    @Column(unique = true)
+    private String nickName;
+    private boolean muted;
+
+    public Guest() {
+    }
+    public Guest(String nickName) {
+        this.nickName = nickName;
+        this.muted=false;
+    }
+
+    public String getNickName() {
+        return nickName;
+    }
+    public boolean isMuted() {
+        return muted;
+    }
+    public void setMuted(boolean muted) {
+        this.muted = muted;
+    }
+}
